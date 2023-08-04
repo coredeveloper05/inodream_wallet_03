a0100c6056a3248f77b3f5a725ac19ce864b5dbe
coredeveloper01 <coremdeveloper01@gmail.com>
2023-08-04 14:52:47 +0900

    2023-08-04 출금 비밀번호 초기화 퍼블리싱

--- app/src/main/java/io/inodream/wallet/App.java
+++ app/src/main/java/io/inodream/wallet/App.java
@@ -1,10 +1,18 @@
 package io.inodream.wallet;
 
 import android.app.Application;
+import android.view.View;
 
 import com.blankj.utilcode.util.Utils;
 
+import java.util.concurrent.TimeUnit;
+
 import io.inodream.wallet.refer.retrofit.data.TokenInfosData;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * <pre>
@@ -19,6 +27,11 @@ public class App extends Application {
 
    private static App mApp;
    private TokenInfosData mTokenInfosData;
+
+   public static final int REQUEST_VALIDATOR_TIME = 300;
+
+   public int requestValidatorTime = REQUEST_VALIDATOR_TIME;
+   private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
    @Override
    public void onCreate() {
@@ -37,5 +50,23 @@ public class App extends Application {
 
    public void setTokenInfosData(TokenInfosData tokenInfosData) {
       mTokenInfosData = tokenInfosData;
+   }
+
+   public void tickerRequestValidatorTime() {
+      if(requestValidatorTime == REQUEST_VALIDATOR_TIME) {
+         compositeDisposable.add(Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+                 .observeOn(Schedulers.io())
+                 .subscribe(
+                         (item) -> {
+                            requestValidatorTime--;
+                            if(requestValidatorTime == 0) initRequestValidatorTime();
+                         }
+                 ));
+      }
+   }
+
+   private void initRequestValidatorTime() {
+      requestValidatorTime = REQUEST_VALIDATOR_TIME;
+      compositeDisposable.clear();
    }
 }
--- app/src/main/java/io/inodream/wallet/fragments/SettingFragment.kt
+++ app/src/main/java/io/inodream/wallet/fragments/SettingFragment.kt
@@ -8,19 +8,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import io.inodream.wallet.App
 import io.inodream.wallet.R
 import io.inodream.wallet.activitys.SetWalletInitActivity
 import io.inodream.wallet.activitys.TokenSendRequestActivity
 import io.inodream.wallet.databinding.FragmentSettingBinding
 import io.inodream.wallet.util.UserManager
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class SettingFragment : BaseFragment() {
 
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var withdrawalInitDialog: AlertDialog
+    private var withdrawalInitDialog: AlertDialog? = null
+    private var requestValidatorTime: TextView? = null
+    private var withdrawalInitView: View? = null
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,18 +58,58 @@ class SettingFragment : BaseFragment() {
     }
 
     private fun loadDialog() {
-        val withdrawalInitView = LayoutInflater.from(requireContext()).inflate(R.layout.view_withdrawal_init, null)
 
-        withdrawalInitDialog = AlertDialog.Builder(requireContext())
-            .setView(withdrawalInitView)
-            .create()
+        if(withdrawalInitDialog == null) {
+            withdrawalInitView = LayoutInflater.from(requireContext()).inflate(R.layout.view_withdrawal_init, null)
 
-        withdrawalInitDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            withdrawalInitDialog = AlertDialog.Builder(requireContext())
+                .setView(withdrawalInitView)
+                .create()
 
-        withdrawalInitView.findViewById<TextView>(R.id.initCloseBtn)?.setOnClickListener {
-            withdrawalInitDialog?.dismiss()
+            withdrawalInitDialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            requestValidatorTime = withdrawalInitView?.findViewById<TextView>(R.id.requestValidatorTime)
+
+            withdrawalInitView?.findViewById<ConstraintLayout>(R.id.requestCredentialBtn)?.setOnClickListener {
+                requestValidatorTime?.visibility = View.VISIBLE
+
+                if (App.getInstance().requestValidatorTime == App.REQUEST_VALIDATOR_TIME) {
+                    App.getInstance().tickerRequestValidatorTime()
+                    checkRequestCredentialTime()
+                } else {
+                    Toast.makeText(requireContext(), getString(R.string.request_credential_error_message, App.getInstance().requestValidatorTime), Toast.LENGTH_SHORT).show()
+                }
+            }
+            withdrawalInitView?.findViewById<LinearLayout>(R.id.initBtn)?.setOnClickListener {
+                withdrawalInitDialog?.dismiss()
+            }
+
+            withdrawalInitDialog?.show()
+        } else {
+            withdrawalInitDialog?.show()
         }
 
-        withdrawalInitDialog?.show()
+        if(App.getInstance().requestValidatorTime != App.REQUEST_VALIDATOR_TIME) {
+            checkRequestCredentialTime()
+            requestValidatorTime?.visibility = View.VISIBLE
+        }
+    }
+
+    private fun checkRequestCredentialTime() {
+        compositeDisposable.add(Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                requestValidatorTime?.text = "${App.getInstance().requestValidatorTime} 초"
+
+                if(App.getInstance().requestValidatorTime <= 0 || App.getInstance().requestValidatorTime == App.REQUEST_VALIDATOR_TIME) {
+                    initRequestCredential()
+                }
+            })
+    }
+
+    private fun initRequestCredential() {
+        compositeDisposable.clear()
+        requestValidatorTime?.visibility = View.INVISIBLE
     }
 }
